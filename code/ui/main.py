d5ea447e2e6807496131f1b1c4b9798a90ecf269
Duan1-ux <2972352594@qq.com>
2025-09-08 17:19:35 +0800

    Add files via upload

---
+++ code/ui/main.py
@@ -0,0 +1,21 @@
+
+# -*- coding: UTF-8 -*-
+import sys
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import Qt, QCoreApplication
+from aiwindow import AutonomousDrivingUI
+
+if __name__ == "__main__":
+    # 启用高DPI缩放
+    QCoreApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+    
+    app = QApplication(sys.argv)
+    
+    # 设置应用程序样式
+    app.setStyle('Fusion')
+    
+    window = AutonomousDrivingUI()
+    window.show()
+    
+    sys.exit(app.exec_())
+
