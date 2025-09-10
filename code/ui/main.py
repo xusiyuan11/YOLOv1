f94299b9430ee3613b19bf6f8eeca39ad825f674
easymoneysniper114 <1513058312@qq.com>
2025-09-10 16:23:09 +0800

    Update main.py

--- code/ui/main.py
+++ code/ui/main.py
@@ -1,4 +1,3 @@
-
 # -*- coding: UTF-8 -*-
 import sys
 from PyQt5.QtWidgets import QApplication
@@ -8,14 +7,13 @@ from aiwindow import AutonomousDrivingUI
 if __name__ == "__main__":
     # 启用高DPI缩放
     QCoreApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
-    
+
     app = QApplication(sys.argv)
-    
+
     # 设置应用程序样式
     app.setStyle('Fusion')
-    
+
     window = AutonomousDrivingUI()
     window.show()
-    
-    sys.exit(app.exec_())
 
+    sys.exit(app.exec_())
